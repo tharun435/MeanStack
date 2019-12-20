003174a9b43781033ffeb5218f9cb9eb71a1ac3f
THARUN KUMAR TALLAPALLI <43419897+tharun435@users.noreply.github.com>
2019-12-20 15:13:18 +0530

    firts change

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express')
 const app=express()
-const port=3000
+const port=process.env.PORT || 3000
 app.get('/',(req,res)=> res.send('JAveed is good boy'))
 app.listen(port,()=>console.log("connection extablishied..!!"))
